347312cfead5c9d335a6db6ece693aef37e4dacb
sachinacadsigma <sachinsb@acadsigma.com>
2024-12-28 00:26:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def hello():
     return "Hi"
 
+from pto_upload_file import process_document
+@app.route('/process_doc', methods=['POST'])
+def call_process_document():
+    return process_document()
+
+from pto_erp_call import get_erp_data
+@app.route('/erp_call', methods=['POST'])
+def call_get_erp_data():
+    return get_erp_data()
+
+from pto_calculation import calculate_vacation
+@app.route('/calculate_vacation_hours', methods=['POST'])
+def call_calculate_vacation():
+    return calculate_vacation()
+
 if __name__ == "__main__":
     app.run(debug=True)
